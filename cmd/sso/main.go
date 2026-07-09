1ea57d81870e9545c489c46d169133fb346f5148
Yaroslav <fokrivosheev@mail.ru>
2026-07-10 00:58:10 +0500

    feat: add slog

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,12 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg)
+
+	log.Info("starting application")
+
+}
+
+func setupLogger(cfg *config.Config) *slog.Logger {
+	var log *slog.Logger
+
+	switch cfg.Env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
